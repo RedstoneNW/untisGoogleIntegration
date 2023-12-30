5d338ca978778e964a2550481f4532b8b0abf063
RedstoneNW <redstone2304nw@gmail.com>
2023-12-30 05:18:49 +0100

    Specified inputs

--- src/main/java/dataHandlement/GoogleCalendarAPI.java
+++ src/main/java/dataHandlement/GoogleCalendarAPI.java
@@ -18,7 +18,10 @@ import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.google.api.services.calendar.model.Events;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
